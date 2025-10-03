c3232ebd1f7f12254126c3bdd2022f7375c217a9
FreddyGomezO <freddyismaelgomez1@gmail.com>
2025-10-02 20:39:26 -0500

    Primer commit

---
+++ src/main/java/com/ista/springboot/app/AzurePruebaApplication.java
@@ -0,0 +1,13 @@
+package com.ista.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurePruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurePruebaApplication.class, args);
+	}
+
+}
